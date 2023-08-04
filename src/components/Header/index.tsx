966c2b008cd32af5ce3cfba5a23d7d9ffdf02b0c
Erick dos Santos <erickspy2003@gmail.com>
2023-08-04 13:19:07 -0300

    fixed: header font-family

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export function Header() {
     return (
         <header className="w-full h-20 flex flex-row items-center justify-center bg-zinc-950 shadow-2xl">
             <Logo />
-            <h1 className="text-slate-600 text-6xl font-bold">Erick Book</h1>
+            <h1 className="text-slate-600 text-6xl font-bold font-JetBrains">Erick Book</h1>
         </header>
     )
 }
