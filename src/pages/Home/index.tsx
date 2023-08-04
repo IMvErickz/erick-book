736543b9cf6faede08946109154be6f5def138cc
Erick dos Santos <erickspy2003@gmail.com>
2023-08-04 18:52:11 -0300

    icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,17 +17,19 @@ export function Home() {
     const icons = [
         { icon: JavascriptIcon },
         { icon: TypescriptIcon },
-        { icon: ReactIcon },
-        { icon: NativeIcon },
         { icon: NodeIcon },
         { icon: PrismaIcon },
+    ]
 
+    const reactIcons = [
+        { icon: ReactIcon },
+        { icon: NativeIcon },
     ]
 
     return (
-        <main className="w-screen bg-zinc-900 flex flex-col">
+        <main className="w-screen bg-zinc-900 flex flex-col gap-y-8">
             <Header />
-            <section className="w-full flex flex-row items-center justify-center gap-x-60 pt-24">
+            <section className="w-full flex flex-row items-center justify-center gap-x-52">
                 <p className="text-white font-JetBrains text-5xl text-center">
                     Seja bem vindo ao meu <br />
                     espaço dedicado aos <br />
@@ -40,20 +42,27 @@ export function Home() {
                     Full-Stack
                 </p>
             </section>
-            <section className="w-full flex flex-col items-center justify-center pt-36">
+            <section className="w-full flex flex-col items-center justify-center">
                 <p className="text-white font-JetBrains text-4xl text-center">
                     Tecnologias Usadas: <br />
                     (em meus projetos pessoais)
                 </p>
             </section>
-            <section className="w-full flex flex-row items-center justify-center gap-x-40 pt-14">
+            <section className="w-full flex flex-row items-center justify-center gap-x-40">
+                {reactIcons.map(e => {
+                    return (
+                        <img src={e.icon} alt="Não" />
+                    )
+                })}
+            </section>
+            <section className="w-full flex flex-row items-center justify-center gap-x-40">
                 {icons.map(e => {
                     return (
                         <img src={e.icon} alt="Não" />
                     )
                 })}
             </section>
-            {/* <section className="w-full flex flex-row items-center justify-center">
+            {/* <section className="flex flex-row items-center justify-center">
                 <Button
                     text="Contato"
                     backgroundColor="bg-green-600"
